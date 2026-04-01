b469adfbfdbc5f218a1f595cb7899a3b64e4f8cf
PRANEETPRASAD <prasadpraneet203@gmail.com>
2026-04-01 11:07:20 +0530

    push the repo

---
+++ script.js
@@ -0,0 +1,9 @@
+function calculate() {
+  let V = document.getElementById("voltage").value;
+  let R = document.getElementById("resistance").value;
+
+  let I = V / R;
+
+  document.getElementById("result").innerText =
+    "Current = " + I + " Amps";
+}
